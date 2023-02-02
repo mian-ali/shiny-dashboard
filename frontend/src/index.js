4e9e68dc60e5b98f46cf012484e6eb717d4b72d1
Ali Ahmad <mian41935@gmail.com>
2023-02-02 23:08:43 +0500

    Basic redux @toolKit setup ✅ for light and dark

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import globalReducer from 'states';
+
+export const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
