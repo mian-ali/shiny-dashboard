ceee6a86eeea9ec53c03f60b5bd3c55d8a59df42
Ali Ahmad <mian41935@gmail.com>
2023-04-17 00:05:34 +0500

    Customer data show inside client side 🚀� #14

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { themeSettings } from 'theme';
 import Layout from 'scenes/layout';
 import Dashboard from 'scenes/dashboard';
 import Products from 'scenes/products';
+import Customers from 'scenes/customers';
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -22,6 +23,8 @@ function App() {
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+
 
               
             </Route>
