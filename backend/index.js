f2bb24d4fe350d82ba27fc5b9613b8e99e9c8ac7
Ali Ahmad <mian41935@gmail.com>
2023-04-16 16:16:51 +0500

    Create  RTK Query for getting products  and products and products stats data injecting mock data into MongoDB Database 🚀🚀 #13 product page is completed

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,9 @@ import salesRoutes from './routes/sales.js';
 // data imports
 
 import User from './models/User.js';
-import { dataUser } from './data/index.js';
+import Product from './models/Product.js';
+import ProductStat from './models/ProductStat.js';
+import { dataUser, dataProduct, dataProductStat } from './data/index.js';
 
 /* CONFIGRATION */
 dotenv.config();
@@ -51,6 +53,8 @@ mongoose
     );
 
     /* ONLY ADD DATA ONE TIME */
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
     // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
--- frontend/src/state/api.js
+++ frontend/src/state/api.js
@@ -3,13 +3,18 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: ' http://localhost:5000' }),
   reducerPath: 'adminApi',
-  tagTypes: ['User'],
+  tagTypes: ['User', 'Products'],
   endpoints: (builder) => ({
     getUsers: builder.query({
       query: (id) => `general/user/${id}`,
       providesTags: ['User'],
     }),
+
+    getProducts: builder.query({
+      query: () => `client/products`,
+      providesTags: ['Products'],
+    }),
   }),
 });
 
-export const { useGetUsersQuery } = apiSlice;
+export const { useGetUsersQuery, useGetProductsQuery } = apiSlice;
