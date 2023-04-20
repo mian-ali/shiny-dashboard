c95393e62a43d76cd0292802969d2839f38a8fb6
Ali Ahmad <mian41935@gmail.com>
2023-04-21 01:25:10 +0500

    product data Transaction backend Api call finished 🚀🚀✅✅

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,13 @@ import salesRoutes from './routes/sales.js';
 import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
-import { dataUser, dataProduct, dataProductStat } from './data/index.js';
+import Transaction from './models/Transactions.js';
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from './data/index.js';
 
 /* CONFIGRATION */
 dotenv.config();
@@ -55,6 +61,7 @@ mongoose
     /* ONLY ADD DATA ONE TIME */
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
